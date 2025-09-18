ed3632f954aa73ad28aaf2b2a42500d6184969ea
vaibhavs321 <vaibhavs@teknobuilt.com>
2025-09-18 20:30:16 +0530

    added class5

--- src/test/java/testLayer/TestClass4.java
+++ src/test/java/testLayer/TestClass4.java
@@ -8,11 +8,11 @@ import org.testng.annotations.Test;
 import testBase.DriverManager;
 import testBase.TestBase;
 
-public class TestClass1 extends TestBase {
+public class TestClass4 extends TestBase {
 	
 	@Test
-	public void testMethod1() throws InterruptedException {
-		Thread.sleep(300);
+	public void testMethod7() throws InterruptedException {
+		Thread.sleep(3000);
 		WebDriver testDriver = DriverManager.getDriver();
 		testDriver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
 		testDriver.findElement(By.xpath("//input[@placeholder='Search for products, brands and more']")).sendKeys("mobiles"+Keys.ENTER);
@@ -21,7 +21,7 @@ public class TestClass1 extends TestBase {
 		
 	}
 	@Test
-	public void testMethod2() throws InterruptedException {
+	public void testMethod8() throws InterruptedException {
 		WebDriver testDriver = DriverManager.getDriver();
 		Thread.sleep(3000);
 		testDriver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
