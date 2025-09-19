b4c0909430267ca6766ec8b9641018d386820252
vaibhavs321 <vaibhavs@teknobuilt.com>
2025-09-20 01:28:34 +0530

    Chrome options added

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,7 +19,14 @@ public class TestBase {
 
 		switch (browser.toLowerCase()) {
 		case "chrome":
-			driverInstance = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless=new"); // ✅ run in headless mode (needed on GitHub Actions)
+			options.addArguments("--no-sandbox"); // ✅ required in CI
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--disable-gpu");
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--user-data-dir=/tmp/chrome-profile"); // ✅ unique dir for profile
+			driverInstance = new ChromeDriver(options);
 			break;
 		case "edge":
 			driverInstance = new EdgeDriver();
