ddb64962207b0e5cff70d4018ec547623fdec911
vaibhavs321 <vaibhavs@teknobuilt.com>
2025-09-20 01:11:35 +0530

    Add CI pipeline

--- src/main/java/utility/ListenerSetup.java
+++ src/main/java/utility/ListenerSetup.java
@@ -8,24 +8,20 @@ public class ListenerSetup implements ITestListener {
 	@Override
 	public void onTestStart(ITestResult result) {
 		System.out.println("Test Started: "+result.getMethod().getMethodName());
-		ITestListener.super.onTestStart(result);
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		System.out.println("Test Passed: "+result.getMethod().getMethodName());
-		ITestListener.super.onTestSuccess(result);
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
 		System.out.println("Test Failed: "+result.getMethod().getMethodName());
-		ITestListener.super.onTestFailure(result);
 	}
 	@Override
 	public void onTestSkipped(ITestResult result) {
 		System.out.println("Test Skipped: "+result.getMethod().getMethodName());
-		ITestListener.super.onTestFailure(result);
 	}
 	
 
