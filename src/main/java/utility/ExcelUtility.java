e52bda488e8f66d1df1b2a8a8523da16d7064afe
vaibhavs321 <vaibhavs@teknobuilt.com>
2025-09-20 03:28:33 +0530

    ci-cd test-2

--- src/main/java/utility/ExcelUtility.java
+++ src/main/java/utility/ExcelUtility.java
@@ -5,6 +5,8 @@ public class ExcelUtility {
 	public static void main(String[] args) {
 
 		System.out.println("CI-CD pipeline checking is successed");
+		System.out.println("CI-CD pipeline checking is successed");
+
 	}
 
 }
